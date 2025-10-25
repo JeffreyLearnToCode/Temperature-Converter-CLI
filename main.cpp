b3a75592030b76748b37aa7ee5758323aa485da4
Jeffrey Abraham <jeffrey@abrahamjeffrey.com>
2025-10-25 15:22:11 -0400

    Refactor code by learning about more about cin, enum and for loop

--- main.cpp
+++ main.cpp
@@ -1,83 +1,99 @@
 #include <iostream>
 #include <string>
+#include <limits>
 #include <array>
 
-
-class TemperatureConverter {
-    const std::array<std::string, 3> temperatureUnits = {"Celsius", "Fahrenheit", "Kelvin"};
-    const std::array<std::string, 3> temperatureUnitChar = {"C", "F", "K"};
-    std::string userPickedTempString;
-    std::string desiredPickedTempString;
-    std::string tempChar;
-    double userPickedTemp = 0;
-
-    void outputTemperatureChoose(const int &offset = 0) const {
-        for (int i = 0; i < this->temperatureUnits.size(); i++) {
-            std::cout << (i+offset) << "."<< this->temperatureUnits[i] << std::endl;
-        }
-    }
-
-    void validateUserPicked(const int &offset = 0) const {
-        if (this->userPickedTemp >= (this->temperatureUnits.size() + offset) ||
-            this->userPickedTemp <= (this->temperatureUnits.size() - this->temperatureUnits.size())){
-                std::cout << "You entered an invalid desired temperature unit." << std::endl;
-            }
-    }
-
-    void userChoose() {
-        int userPickedTempUnit;
-        int userDesiredTempUnit;
-
-
-        std::cout << "Hello, please enter the current temperature: ";
-        std::cin >> userPickedTemp;
-
-        this->outputTemperatureChoose(1);
-        std::cout << "What is the temperature unit: ";
-        std::cin >> userPickedTempUnit;
-        this->validateUserPicked(1);
-
-        this->outputTemperatureChoose(1);
-        std::cout << "What is the desired temperature unit: ";
-        std::cin >> userDesiredTempUnit;
-        this->validateUserPicked(1);
-
-        userPickedTempString = this->temperatureUnits[userPickedTempUnit - 1];
-        desiredPickedTempString = this->temperatureUnits[userDesiredTempUnit - 1];
-        tempChar = this->temperatureUnitChar[userDesiredTempUnit - 1];
-    }
-
-    void outputDesiredTemperature() const {
-        if (userPickedTempString == desiredPickedTempString) {
-            std::cout << "Your desired temperature: " << userPickedTemp << "°" << tempChar<< std::endl;
-        }
-
-        if (userPickedTempString == "Celsius" && desiredPickedTempString == "Fahrenheit") {
-            std::cout << "Your desired temperature: " << (userPickedTemp * 9 / 5) + 32 << "°" << tempChar<< std::endl;
-        } else if (userPickedTempString == "Fahrenheit" && desiredPickedTempString == "Celsius") {
-            std::cout << "Your desired temperature: " << (userPickedTemp - 32) * 5 / 9 << "°" << tempChar<< std::endl;
-        } else if (userPickedTempString == "Celsius" && desiredPickedTempString == "Kelvin") {
-            std::cout << "Your desired temperature: " << userPickedTemp + 273.15 << "°" << tempChar<< std::endl;
-        } else if (userPickedTempString == "Kelvin" && desiredPickedTempString == "Celsius") {
-            std::cout << "Your desired temperature: " << userPickedTemp - 273.15 << "°" << tempChar<< std::endl;
-        } else if (userPickedTempString == "Fahrenheit" && desiredPickedTempString == "Kelvin") {
-            std::cout << "Your desired temperature: " << (userPickedTemp - 32) * 5 / 9 + 273.15 << "°" << tempChar<< std::endl;
-        } else if (userPickedTempString == "Kelvin" && desiredPickedTempString == "Fahrenheit") {
-            std::cout << "Your desired temperature: " << (userPickedTemp - 273.15) * 9 / 5 + 32 << "°" << tempChar<< std::endl;
-        }
-    }
-
-
-public:
-    TemperatureConverter() {
-        userChoose();
-        outputDesiredTemperature();
-    };
+enum Unit {
+    C = 1,
+    F,
+    K
 };
+
+constexpr double ZERO_C_TO_KELVIN = 273.15;
+
+constexpr std::array<std::string_view, 3> unitsName = {"Celsius", "Fahrenheit", "Kelvin"};
+constexpr std::array<std::string_view, 3> unitsSymbol = {"C", "F", "K"};
+
+void printUnitNames(const int offset = 0) {
+    for (int i = 0; i < static_cast<int>(unitsName.size()); i++) {
+        std::cout << (i+offset) << "." << unitsName[i] << std::endl;
+    }
+}
+
+int readCurrentUnitPick(const int low, const int high) {
+    for (;;) {
+
+        if (int unit; std::cin >> unit
+            && unit >= low && unit <= high) return unit;
+
+        // Error handling
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Please enter a valid temperature unit between " << low << " and " << high << ": ";
+    }
+}
+
+
+double readCurrentTempPick() {
+    for (;;) {
+        if (double d; std::cin >> d) return d;
+
+        // Error handling
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Please enter a valid temperature: ";
+    }
+}
+
+constexpr double toKelvin(const Unit &u, const double &t) {
+    switch (u) {
+        case C: return (t + ZERO_C_TO_KELVIN);
+        case F: return (t - 32) * 5/9 + ZERO_C_TO_KELVIN;
+        case K: return t;
+    }
+    return t;
+}
+
+constexpr double fromKelvin(const Unit &u, const double &k) {
+    switch (u) {
+        case C: return (k - ZERO_C_TO_KELVIN);
+        case F: return (k - ZERO_C_TO_KELVIN) * 9/5 + 32;
+        case K: return k;
+    }
+    return k;
+}
+
+constexpr double convert(const double &temp, const Unit &to, const Unit &from) {
+    if (to == from) return temp;
+
+    return fromKelvin(to, toKelvin(from, temp));
+}
 
 
 int main() {
-    TemperatureConverter temperatureConverter;
+    // Get current temperature picked
+    std::cout << "Please enter the current temperature: ";
+    const double userCurrentTempPick = readCurrentTempPick();
+
+    // This is used to set the desired offset for the printed option
+    constexpr int offset = 1;
+
+    // Get current temperature unit
+    printUnitNames(offset);
+    std::cout << "Please picked the current temperature unit: ";
+    const int userCurrentTempUnit = readCurrentUnitPick(offset, unitsName.size());
+
+    // Get desired temperature unit
+    printUnitNames(offset);
+    std::cout << "Please picked the desired temperature unit: ";
+    const int userDesiredTempUnit = readCurrentUnitPick(offset, unitsName.size());
+
+    const Unit to = static_cast<Unit>(userDesiredTempUnit);
+    const Unit from = static_cast<Unit>(userCurrentTempUnit);
+
+    // Convert temperature
+    const double convertTemperature = convert(userCurrentTempPick, to, from);
+    std::cout << "Your desired temperature: " << convertTemperature << "°" << unitsSymbol[userDesiredTempUnit - 1] << std::endl;
 
     return 0;
 }
