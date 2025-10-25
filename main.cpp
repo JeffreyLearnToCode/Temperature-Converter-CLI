206d79c74da202131409aabb9f0ea1c682f86d7d
Jeffrey Abraham <jeffrey@abrahamjeffrey.com>
2025-10-25 12:49:35 -0400

    Added ways to catch input error from user when picking the temperature unit

--- main.cpp
+++ main.cpp
@@ -18,11 +18,23 @@ int main() {
     std::cout << "What is the temperature unit: ";
     std::cin >> userPickedTempUnit;
 
+    if (userPickedTempUnit >= (temperatureUnit.size() + 1) ||
+        userPickedTempUnit <= (temperatureUnit.size() - temperatureUnit.size())) {
+        std::cout << "You entered an invalid desired temperature unit." << std::endl;
+        return 0;
+    }
+
     for (int i = 0; i < temperatureUnit.size(); i++) {
         std::cout << (i+1) << "."<< temperatureUnit[i] << std::endl;
     }
     std::cout << "What is the desired temperature unit: ";
     std::cin >> userDesiredTempUnit;
+
+    if (userDesiredTempUnit >= (temperatureUnit.size() + 1) ||
+        userDesiredTempUnit <= (temperatureUnit.size() - temperatureUnit.size())) {
+        std::cout << "You entered an invalid desired temperature unit." << std::endl;
+        return 0;
+    }
 
 
 
